ad5a9045a0234a159971a3f4c537bbd2dca2ca26
Hui1499577370 <114322419+Hui1499577370@users.noreply.github.com>
2022-09-25 12:56:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oMRQg5sxg3CMhPgjYaWFpS4XdfwY',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'b36HhChCCda5B350RjVydhm7uQ4haA-FVet8fkgHXeM',
+      useTemplateId: 'Lspyy4eS7XQmv2LrwQIkw85a6Ab2xoWtpm4L0rUJ9eg',
       // 所在省份
       province: '安徽',
       // 所在城市
