35ce8e782e93c842a992e9f772ad4e9f783ce6d1
Hui1499577370 <114322419+Hui1499577370@users.noreply.github.com>
2022-09-25 13:07:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oMRQg5sxg3CMhPgjYaWFpS4XdfwY',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'Lspyy4eS7XQmv2LrwQIkw85a6Ab2xoWtpm4L0rUJ9eg',
+      useTemplateId: 'ILvBDX7sQpwfOyCDSNngpGAtQuhaXqTyoDOhVhrMG4Y	',
       // 所在省份
       province: '安徽',
       // 所在城市
@@ -257,7 +257,7 @@ const USER_CONFIG = {
     // 在一起的日子
     { keyword: 'love_day', date: '2020-07-30' },
     // 结婚纪念日
-    { keyword: 'birthday_message', date: '201-06-08' },
+    { keyword: 'birthday_message', date: '2001-06-08' },
     // 退伍日
     { keyword: 'ex_day', date: '2022-09-10' }
     // 你可以不断按格式往下增加
